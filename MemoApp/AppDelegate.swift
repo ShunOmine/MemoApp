8b8ba8f7409d2627e3e7fc78db656f52e9b78942
ShunOmine <shun12baseball@gmail.com>
2019-08-03 10:07:20 +0900

    create memo app

--- MemoApp/AppDelegate.swift
+++ MemoApp/AppDelegate.swift
@@ -13,8 +13,14 @@ class AppDelegate: UIResponder, UIApplicationDelegate {
 
     var window: UIWindow?
 
+    // ViewControllerのTextViewに入力された最後の内容を格納します。
+    var lastText: String?
 
     func application(_ application: UIApplication, didFinishLaunchingWithOptions launchOptions: [UIApplication.LaunchOptionsKey: Any]?) -> Bool {
+        // アプリに保存されている最後の入力内容を読み込み、lastTextに格納します。
+        let userDefaults = UserDefaults.standard
+        lastText = userDefaults.object(forKey: "lastText") as? String
+        
         // Override point for customization after application launch.
         return true
     }
@@ -22,6 +28,10 @@ class AppDelegate: UIResponder, UIApplicationDelegate {
     func applicationWillResignActive(_ application: UIApplication) {
         // Sent when the application is about to move from active to inactive state. This can occur for certain types of temporary interruptions (such as an incoming phone call or SMS message) or when the user quits the application and it begins the transition to the background state.
         // Use this method to pause ongoing tasks, disable timers, and invalidate graphics rendering callbacks. Games should use this method to pause the game.
+        
+        // lastTextに格納されている入力内容をアプリに保存します。
+        let userDefaults = UserDefaults.standard
+        userDefaults.set(lastText, forKey: "lastText")
     }
 
     func applicationDidEnterBackground(_ application: UIApplication) {
